6cd44b32ddd2aa174b626885e7fed2070accf031
Goldmann1995 <112161978+Goldmann1995@users.noreply.github.com>
2024-02-15 16:08:20 +0800

    Update openAI.ts
    reset

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -8,7 +8,7 @@ const genAI = apiBaseUrl
   : new GoogleGenerativeAI(apiKey)
 
 export const startChatAndSendMessageStream = async(history: ChatMessage[], newMessage: string) => {
-  const model = genAI.getGenerativeModel({ model: 'gemini-pro-vision' })
+  const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
 
   const chat = model.startChat({
     history: history.map(msg => ({
